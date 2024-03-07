f296f45fdbefb69b1c0fa21c8d4edbdf42e2f94a
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-03-07 17:47:22 +0200

    the package.json file was changed and the index.js file was added
    "the package.json file was changed and the index.js file was added"

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('Hello WORLD');
+import fs from 'fs/promises';
+
+const func = async () => {
+  const filePath = './constant/10m.txt';
+  const data = await fs.readFile(filePath, 'utf-8');
+
+  const numbers = data.trim().split('\n').map(Number);
+  console.log('Array numbers', numbers);
+
+  // for (let i = 1; i < numbers.length; i += 1) {
+  //   let maxNumber = numbers[0];
+  //   let minNumber = numbers[0];
+
+  //   if (numbers[i] > maxNumber) {
+  //     maxNumber = numbers[i];
+  //   }
+
+  //   if (numbers[i] < minNumber) {
+  //     minNumber = numbers[i];
+  //   }
+  //   console.log('maxNumber', maxNumber);
+  //   console.log('minNumber', minNumber);
+  // }
+
+  const sum = numbers.reduce((prevValue, number) => prevValue + number, 0);
+  const average = Math.round(sum / numbers.length);
+  console.log('average', average);
+
+  const minNumber = numbers.reduce(
+    (prevValue, minNum) => (minNum < prevValue ? minNum : prevValue),
+    0
+  );
+  console.log('minNumber', minNumber);
+
+  const maxNumber = numbers.reduce(
+    (prevValue, maxNum) => (maxNum > prevValue ? maxNum : prevValue),
+    0
+  );
+  console.log('maxNumber', maxNumber);
+};
+
+func();
